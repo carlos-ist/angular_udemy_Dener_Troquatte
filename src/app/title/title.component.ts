0fccde340fc00e3a4642a0c6c9d431414cfd151a
jose-ist <carlos.work@outlook.com>
2022-07-29 11:56:51 -0300

    Title Component continuação

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TitleComponent implements OnInit {
 
+  public title: string = "Bem vindo ao Angular!";
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
